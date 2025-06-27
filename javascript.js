10164029c9e136c04c1defc36c559ca194343c8d
HadiBdeir <ophadi.bdeir@gmail.com>
2025-06-27 19:58:35 -0400

    Added the functionality of asking wether or not you want to start the game. The game also asks if you want to continue after playing a round. Also added a scoreboard to keep track of the number of times the the player and the computer one.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,24 @@
-console.log("Hello world!");
-
 let start = false;
 
 const options = ["rock", "paper", "scissors"];
 let answer;
 let computerAnswer;
+let yourScore = 0;
+let computerScore = 0;
+let result;
+
+const askToPlay = () => {
+  let playAgain = prompt("Do you want to play? y or n!");
+  if (playAgain.toLowerCase() === "y" || playAgain.toLowerCase() === "yes") {
+    return playerChoice();
+  } else if (playAgain.toLowerCase() === "n" || playAgain.toLowerCase() === "no") {
+    console.log("I guess you dont want to :(");
+    return;
+  } else {
+    prompt("Unknown value, please type again.");
+    return askToPlay();
+  }
+};
 
 const playerChoice = () => {
   answer = prompt("Pick rock, paper, or scissors").toLowerCase();
@@ -14,12 +28,13 @@ const playerChoice = () => {
   } else {
     console.log("Player choice: " + answer);
   }
-  return;
+  return getComputerChoice();
 };
 
 const getComputerChoice = () => {
   computerAnswer = Math.floor(Math.random() * options.length);
-  return console.log("Robot choice: " + options[computerAnswer]);
+  console.log("Robot choice: " + options[computerAnswer]);
+  return battle();
 };
 
 function battle() {
@@ -28,20 +43,25 @@ function battle() {
     (answer === "paper" && options[computerAnswer] === "rock") ||
     (answer === "scissors" && options[computerAnswer] === "paper")
   ) {
-    return console.log("Congrats, you win!");
+    yourScore++;
+    let result = console.log("Congrats, you win!");
   } else if (
     (answer === "rock" && options[computerAnswer] === "paper") ||
     (answer === "paper" && options[computerAnswer] === "scissors") ||
     (answer === "scissors" && options[computerAnswer] === "rock")
   ) {
-    return console.log("You lost :(");
+    computerScore++;
+    let result = console.log("You lost :(");
   } else if (answer === options[computerAnswer]) {
-    return console.log("It's a tie");
+    let result = console.log("It's a tie");
   } else {
-    return console.log("Error, somethings not right.");
+    let result = console.log("Error, somethings not right.");
   }
+  console.log(
+    `Your score is: ${yourScore}, the opponents score is: ${computerScore}`
+  );
+  askToPlay();
+  return result;
 }
 
-playerChoice();
-getComputerChoice();
-battle();
+askToPlay();
